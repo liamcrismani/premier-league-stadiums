5ae3c01f2f628700ba4527c6a674d4d5d4b20805
liamcrismani <35870151+liamcrismani@users.noreply.github.com>
2025-03-23 15:08:46 +0000

    - update field names - update foreign/primary keys - load extensions

--- db_create.sql
+++ db_create.sql
@@ -1,31 +1,38 @@
+PRAGMA trusted_schema = 1;
+
+SELECT load_extension('mod_spatialite');
+
+SELECT InitSpatialMetaData();
+        
 CREATE TABLE IF NOT EXISTS "clubs" (
-	"id" INTEGER NOT NULL UNIQUE,
+	"club_id" INTEGER NOT NULL UNIQUE,
 	"name" TEXT NOT NULL,
 	"established" TEXT,
-	PRIMARY KEY("id")
+	PRIMARY KEY("club_id")
 );
 
 CREATE TABLE IF NOT EXISTS "stadiums" (
-	"id" INTEGER NOT NULL UNIQUE,
+	"stadium_id" INTEGER NOT NULL UNIQUE,
 	"name" TEXT NOT NULL,
-	"peak_capacity" INTEGER,
-	"current_capacity" INTEGER,
-	"club_id" INTEGER NOT NULL,
+	"capacity" INTEGER,
+	"club_id" INTEGER,
 	"opened" TEXT,
 	"closed" TEXT,
-    -- TODO: convert to geometry (point)
-	"geometry" TEXT,
-	PRIMARY KEY("id"),
-	FOREIGN KEY ("club_id") REFERENCES "clubs"("id")
+	"longitude" NUMERIC,
+	"latitude" NUMERIC,
+	PRIMARY KEY("stadium_id"),
+	FOREIGN KEY ("club_id") REFERENCES "clubs"("club_id")
 	ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
+SELECT AddGeometryColumn('stadiums', 'geom', 3857, 'POINT', 'XY');
+
 CREATE TABLE IF NOT EXISTS "tickets" (
 	"id" INTEGER NOT NULL UNIQUE,
-	"stadium_id" INTEGER NOT NULL,
-	"season" TEXT NOT NULL,
-    "avg_price" NUMERIC NOT NULL,
+	"club_id" INTEGER NOT NULL,
+	"cheapest" TEXT NOT NULL,
+	"steapest"  TEXT NOT NULL,
 	PRIMARY KEY("id"),
-	FOREIGN KEY ("stadium_id") REFERENCES "stadiums"("id")
+	FOREIGN KEY ("club_id") REFERENCES "clubs"("club_id")
 	ON UPDATE CASCADE ON DELETE NO ACTION
 );
