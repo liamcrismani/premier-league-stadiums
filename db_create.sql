cd446ba0bbeb76744a426749f1896397159e9a92
liamcrismani <35870151+liamcrismani@users.noreply.github.com>
2025-03-23 17:22:33 +0000

    convert to postgres/duckdb syntax

--- db_create.sql
+++ db_create.sql
@@ -1,38 +1,38 @@
-PRAGMA trusted_schema = 1;
 
-SELECT load_extension('mod_spatialite');
+-- required install
+INSTALL SPATIAL;
 
-SELECT InitSpatialMetaData();
-        
-CREATE TABLE IF NOT EXISTS "clubs" (
-	"club_id" INTEGER NOT NULL UNIQUE,
-	"name" TEXT NOT NULL,
-	"established" TEXT,
-	PRIMARY KEY("club_id")
+-- installing will not load the extension by default
+LOAD SPATIAL;
+ 
+CREATE TABLE IF NOT EXISTS clubs (
+	club_id INTEGER NOT NULL UNIQUE,
+	name VARCHAR NOT NULL,
+	established VARCHAR,
+	PRIMARY KEY(club_id)
 );
 
-CREATE TABLE IF NOT EXISTS "stadiums" (
-	"stadium_id" INTEGER NOT NULL UNIQUE,
-	"name" TEXT NOT NULL,
-	"capacity" INTEGER,
-	"club_id" INTEGER,
-	"opened" TEXT,
-	"closed" TEXT,
-	"longitude" NUMERIC,
-	"latitude" NUMERIC,
-	PRIMARY KEY("stadium_id"),
-	FOREIGN KEY ("club_id") REFERENCES "clubs"("club_id")
+CREATE TABLE IF NOT EXISTS stadiums (
+	stadium_id INTEGER NOT NULL UNIQUE,
+	name VARCHAR NOT NULL,
+	capacity INTEGER,
+	club_id INTEGER,
+	opened VARCHAR,
+	closed VARCHAR,
+	longitude NUMERIC,
+	latitude NUMERIC,
+	geom GEOMETRY,
+	PRIMARY KEY(stadium_id),
+	FOREIGN KEY (club_id) REFERENCES clubs(club_id)
 	ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
-SELECT AddGeometryColumn('stadiums', 'geom', 3857, 'POINT', 'XY');
-
-CREATE TABLE IF NOT EXISTS "tickets" (
-	"id" INTEGER NOT NULL UNIQUE,
-	"club_id" INTEGER NOT NULL,
-	"cheapest" TEXT NOT NULL,
-	"steapest"  TEXT NOT NULL,
-	PRIMARY KEY("id"),
-	FOREIGN KEY ("club_id") REFERENCES "clubs"("club_id")
+CREATE TABLE IF NOT EXISTS tickets (
+	id INTEGER NOT NULL UNIQUE,
+	club_id INTEGER NOT NULL,
+	cheapest VARCHAR NOT NULL,
+	steapest  VARCHAR NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY (club_id) REFERENCES clubs(club_id)
 	ON UPDATE CASCADE ON DELETE NO ACTION
 );
